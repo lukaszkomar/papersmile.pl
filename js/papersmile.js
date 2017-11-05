ea5f58ffcf1a3f3e40868a9e71a8d2abae82736f
lukaszkomar <l_komr@wp.pl>
2017-11-05 12:10:54 +0100

    js img

--- js/papersmile.js
+++ js/papersmile.js
@@ -1,10 +1,5 @@
 $(document).ready(function() {
     $('#karty-img .img-thumbnail').click(function() {
-
-
-        console.log('c');
-        console.log($(this).attr('src'));
-
-        //$('#img-big').attr('src', 'val');
+        $('#img-big').attr('src', $(this).attr('src'));
     });
 });
