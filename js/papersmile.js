c24717c17b5996e536820d3905a732fd3f332465
lukaszkomar <l_komr@wp.pl>
2017-11-12 11:41:48 +0100

    js reload

--- js/papersmile.js
+++ js/papersmile.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
-    if($('#karty-img .img-thumbnail'))
-    {
-        $('#karty-img .img-thumbnail').click(function() {
-            $('#img-big').attr('src', $(this).attr('src'));
+
+    $('#karty-img .img-thumbnail').click(function() {
+        $('#img-big').attr('src', $(this).attr('src'));
+    });
+
+
+
+    /**
+     * Losowe zestawy
+     */
+    var reloadSets = function() {
+
+        var min = 1,
+            max = 4,
+            randNumber =  Math.floor(Math.random() * (max - min) + min + 0.9)
+            ;
+
+        console.log('reloadSets', min, max, randNumber);
+
+        $.each([ 1, 2, 3, 4], function( index, setId ) {
+
+            if (setId == randNumber) {
+                $('#homepage-plakat-' + setId).hide();
+            } else {
+                $('#homepage-plakat-' + setId).show();
+            }
         });
-    }
+    };
+
+
+    /**
+     * Losowe foty mustsee
+     */
+    window.setInterval(reloadSets, 60 * 1000); // every 60 sec
 });
