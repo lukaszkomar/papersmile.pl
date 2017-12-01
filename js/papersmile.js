fd014b0423deb865a07791b9fbffebb49ea0411d
lukaszkomar <l_komr@wp.pl>
2017-12-01 20:51:26 +0100

    js fix

--- js/papersmile.js
+++ js/papersmile.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
         $('#img-big').attr('src', $(this).attr('src'));
     });
 
-    reloadSets('#homepage-karta-' , {{ site.produkty.karty.size }}   , 3);
-    reloadSets('#homepage-plakat-', {{ site.produkty.plakatyy.size }}, 3);
+    reloadSets('#homepage-karta-' , {{ site.produkty.karty.size }}  , 3);
+    reloadSets('#homepage-plakat-', {{ site.produkty.plakaty.size }}, 3);
 
     //window.setInterval(reloadSets, 60 * 1000); // every 60 sec
 });
