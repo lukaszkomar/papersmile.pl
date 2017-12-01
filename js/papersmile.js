e2c612d0b94be5caa1d5ce58eb057e84a070870e
lukaszkomar <l_komr@wp.pl>
2017-12-01 21:41:30 +0100

    gallery

--- js/papersmile.js
+++ js/papersmile.js
@@ -6,7 +6,35 @@ $(document).ready(function() {
         $('#img-big').attr('src', $(this).attr('src'));
     });
 
-    $('.magnific-popup').magnificPopup({delegate: 'a', type:'image'});
+    $('.magnific-popup').magnificPopup({
+        delegate: 'a',
+        type:'image',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true
+        },
+        image: {
+            tError: 'Nie udało sie wczytać obrazu.'
+        }
+    });
+
+    // $('.popup-gallery').magnificPopup({
+    //     delegate: 'a',
+    //     type: 'image',
+    //     tLoading: 'Loading image #%curr%...',
+    //     mainClass: 'mfp-img-mobile',
+    //     gallery: {
+    //         enabled: true,
+    //         navigateByImgClick: true,
+    //         preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    //     },
+    //     image: {
+    //         tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    //         titleSrc: function(item) {
+    //             return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+    //         }
+    //     }
+    // });
 
 
     reloadSets('#homepage-karta-' , {{ site.produkty.karty.size }}  , 3);
