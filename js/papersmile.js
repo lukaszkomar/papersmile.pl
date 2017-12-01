afb165c2a539e01922efd3a0a03ca14cfc2ad822
lukaszkomar <l_komr@wp.pl>
2017-12-01 20:32:09 +0100

    js test

--- js/papersmile.js
+++ js/papersmile.js
@@ -1,10 +1,12 @@
+---
+---
 $(document).ready(function() {
 
     $('#karty-img .img-thumbnail').click(function() {
         $('#img-big').attr('src', $(this).attr('src'));
     });
 
-    reloadSets('#homepage-karta-', 1, 4);
+    reloadSets('#homepage-karta-', 1, {{ site.produkty.karty.size }});
     reloadSets('#homepage-plakat-', 1, 4);
 
     //window.setInterval(reloadSets, 60 * 1000); // every 60 sec
