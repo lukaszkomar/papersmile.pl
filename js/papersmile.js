546dcd7dc22b03dedfd12f5a192cacd26df2f974
lukaszkomar <l_komr@wp.pl>
2017-12-01 21:10:03 +0100

    img big

--- js/papersmile.js
+++ js/papersmile.js
@@ -6,8 +6,12 @@ $(document).ready(function() {
         $('#img-big').attr('src', $(this).attr('src'));
     });
 
+    $('#img-big').magnificPopup({type:'image'});
+
+
     reloadSets('#homepage-karta-' , {{ site.produkty.karty.size }}  , 3);
     reloadSets('#homepage-plakat-', {{ site.produkty.plakaty.size }}, 3);
 
     //window.setInterval(reloadSets, 60 * 1000); // every 60 sec
+
 });
